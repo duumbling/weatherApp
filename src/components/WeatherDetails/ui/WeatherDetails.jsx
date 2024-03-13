35994248f121efd28e30a864ec26055ca942a40a
dumbling <romanskrt@yandex.ru>
2024-03-13 22:54:14 +0300

    fix: change data variable name in WeatherDetails component

--- src/components/WeatherDetails/ui/WeatherDetails.jsx
+++ src/components/WeatherDetails/ui/WeatherDetails.jsx
@@ -4,14 +4,14 @@ import cls from "./WeatherDetails.module.css"
 import { useSelector } from 'react-redux';
 
 export function WeatherDetails() {
-  const weatherData = useSelector((state) => state.currentWeather.value)
+  const {data} = useSelector((state) => state.currentWeather.value)
 
   const [WeatherDetailsData, setWeatherDetailsData] = useState([])
   const [currentWeatherDescription, setCurrentWeatherDescription] = useState('')
 
   useEffect(()=>{
-    let max = Math.max(Math.floor(Number(weatherData?.main?.temp_max)), Math.floor(weatherData?.main?.temp_min))
-    let min = Math.min(Math.floor(Number(weatherData?.main?.temp_max)), Math.floor(weatherData?.main?.temp_min))
+    let max = Math.max(Math.floor(Number(data?.main?.temp_max)), Math.floor(data?.main?.temp_min))
+    let min = Math.min(Math.floor(Number(data?.main?.temp_max)), Math.floor(data?.main?.temp_min))
     const newData = [
       { 
         name: "Мин. температура",
@@ -23,26 +23,26 @@ export function WeatherDetails() {
       },
       {
         name: "Влажность",
-        value: `${weatherData?.main?.humidity || 0}%`
+        value: `${data?.main?.humidity || 0}%`
       },
       {
         name: "Облачность",
-        value: `${weatherData?.clouds?.all || 0}%`
+        value: `${data?.clouds?.all || 0}%`
       },
       {
         name: "Ветер",
-        value: `${Math.floor(weatherData?.wind?.speed || 0)}км/ч`
+        value: `${Math.floor(data?.wind?.speed || 0)}км/ч`
       }
     ]
     
-    if(weatherData) {
-      let weatherArray = weatherData?.weather || [];
+    if(data) {
+      let weatherArray = data?.weather || [];
       let weatherDescription = weatherArray[0]?.description
       setCurrentWeatherDescription(weatherDescription)
     }
     setWeatherDetailsData(newData)
 
-  }, [weatherData])
+  }, [data])
 
 
 
